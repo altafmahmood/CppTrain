6a01fc2bd95b037633570072f695220a3e941224
altafmahmood <altafmahmood@outlook.com>
2015-11-29 23:35:33 +0530

    Added setter and getter

--- PointerCheck.cpp
+++ PointerCheck.cpp
@@ -1,18 +1,32 @@
 /* Author		: Altaf Mahmood
  * Date			: 22 November, 2015
  * Time			: 13:24
- * Description		: A program to learn about string and to play with pointers
+ * Description	: A program to learn about string and to play with pointers
  */
 
 #include <iostream>
-#include <string>
+#include <cstring>
  using namespace std;
 
  class StringPlay{
- 		private:
- 		char str1[] = "Alt25";
+ 	private:
+ 		char name[20];
+ 		int age;
+ 	public:
+ 		void setName(char str){
+ 				strcpy(str,name);
+ 		}
+ 		void setAge(int a){
+ 			age = a;
+ 		}
+ 		void display(){
+ 			cout << name << age << endl;
+ 		}
  };
 
  int main(){
+ 	StringPlay sp;
+ 	char str="Altaf";
+ 	sp.setName(str);
  	return 0;
  }
